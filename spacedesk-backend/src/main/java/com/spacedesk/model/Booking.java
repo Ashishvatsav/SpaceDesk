ed9ca72305e2de18c4a3aee79f834b1f82e2d568
asishnani7 <asishnani7@gmail.com>
2026-04-17 12:35:57 +0530

    BOOKING SYSTEM (CORE MODULE)

--- spacedesk-backend/src/main/java/com/spacedesk/model/Booking.java
+++ spacedesk-backend/src/main/java/com/spacedesk/model/Booking.java
@@ -2,28 +2,40 @@ package com.spacedesk.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "bookings")
 @Data
+@Getter
+@Setter
 public class Booking {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("id")
     private Long id;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
+    @JsonProperty("user")
     private User user;
 
     @ManyToOne
     @JoinColumn(name = "desk_id")
+    @JsonProperty("desk")
     private Desk desk;
 
+    @JsonProperty("startTime")
     private LocalDateTime startTime;
+
+    @JsonProperty("endTime")
     private LocalDateTime endTime;
 
     @Enumerated(EnumType.STRING)
+    @JsonProperty("bookingStatus")
     private BookingStatus bookingStatus;
 }
